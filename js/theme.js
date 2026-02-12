2734086728134ec78e8136bafd6255da4b3113f9
Varul Srivastava <kalki@Varuls-MacBook-Air.local>
2026-02-12 18:14:00 +0530

    added modifications

---
+++ js/theme.js
@@ -0,0 +1,15 @@
+const toggle = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
